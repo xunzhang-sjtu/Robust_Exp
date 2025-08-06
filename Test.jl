917d76fe58de317a48604b07e112fc9da355a030
xunzhang-sjtu <136031617@qq.com>
2025-08-06 10:03:15 +0800

    compute estimation process

--- Test.jl
+++ Test.jl
@@ -3,32 +3,29 @@ using Distributions
 using Optim
 using Random
 using StatsFuns
-using JuMP
-using MosekTools
-include("ETO.jl")
-include("RO.jl")
-include("Data.jl")
+using DataFrames
+using StatsBase
 include("Estimate.jl")
-include("Performance.jl")
-Random.seed!(2)
+# 设置随机种子
+Random.seed!(1)
 
 
-N_u = 1
-N = 2
-S = 100
-S_test = 10000
-K = 10   # 每个产品的选择项数量
-
-max_offdiag = 1
-offdiag_sign = "mix"
+# 模型设置
+N = 3           # 选项数（不含baseline）
+d_u = 1         # u 的维度
+d_p = N         # p 的维度
+S = 5000         # 样本数量
 
 
-A_true, B_true = Generate_Coef(N_u, N,max_offdiag,offdiag_sign);
+a_star = [rand(Uniform(-1, 1), d_u) for n in 1:N]
+b_star = [rand(Uniform(-1, 1), d_p) for n in 1:N]
+a_star = [round.(a; digits=2) for a in a_star]
+b_star = [round.(b; digits=2) for b in b_star]
+println("a_star: ", a_star) 
+println("b_star: ", b_star) 
 
+# Step 2: 生成样本 (u, p)，标准正态分布
+U_train = [randn(d_u) for s in 1:S];
+P_train = [randn(d_p) for s in 1:S];
 
-U_train, P_train = Generate_Feat_Data(N_u, N, S);
-U_test, P_test = Generate_Feat_Data(N_u, N, S_test);
-probs = Calculate_Prob(S,N, U_train, P_train, A_true, B_true);
-choices = Calculate_Choice(S,N,probs);
-
-result = Estimate_MNL_Para(N, N_u, N, U_train, P_train, choices)
+Estimate_MNL_Para(U_train, P_train, S, N, d_u, d_p, a_star, b_star)
