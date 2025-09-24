b58fc922afdb019f8ef12adafd183a2808803d12
xunzhang-sjtu <136031617@qq.com>
2025-09-24 16:34:32 +0800

    update for Product Design

--- Product_Design/Product_Design_Models.jl
+++ Product_Design/Product_Design_Models.jl
@@ -1,4 +1,4 @@
-function Product_Design_Ours_ETO(d,nu0, nu, r0, r, z_input)
+function Product_Design_Ours_ETO(d,nu0, nu, r0, r, C_mat)
 
     model = Model(Mosek.Optimizer)
     set_attribute(model, "QUIET", true)
@@ -11,7 +11,6 @@ function Product_Design_Ours_ETO(d,nu0, nu, r0, r, z_input)
     @variable(model, y[1:d])
     @variable(model, z_int[1:d])
 
-    @constraint(model, sum(x) >= 1)
     @constraint(model, a0 + a1 == 1)
     @constraint(model, [u,a0,a1] in MOI.ExponentialCone())
     @constraint(model, [-v,a1,a0] in MOI.ExponentialCone())
@@ -29,6 +28,8 @@ function Product_Design_Ours_ETO(d,nu0, nu, r0, r, z_input)
         @constraint(model, y[ind] >= x[ind] - (1 - a1))
         @constraint(model, y[ind] >= 0)
     end
+
+    @constraint(model, C_mat * x .== 1)
     @objective(model, Max,r0 * a1 + r' * y)
     optimize!(model)
     status = JuMP.termination_status(model)
@@ -44,7 +45,7 @@ function Product_Design_Ours_ETO(d,nu0, nu, r0, r, z_input)
     return obj_val, x_val, solve_time
 end
 
-function Product_Design_ETO(d,nu0, nu, r0,r, z_input)
+function Product_Design_ETO(d,nu0, nu, r0,r, C_mat)
     model = Model(Mosek.Optimizer)
     set_attribute(model, "QUIET", true)
     # 定义变量
@@ -76,6 +77,8 @@ function Product_Design_ETO(d,nu0, nu, r0,r, z_input)
         @constraint(model, y[ind] >= 0)
     end
     @constraint(model, sum(x) >= 1)
+    # @constraint(model, C_mat * x .== 1)
+
     @objective(model, Max,r0 * a1 + r' * y)
 
     optimize!(model)
@@ -96,7 +99,7 @@ end
 
 
 
-function Robust_Product_Design(N, n_x, n_c, gamma, psi_lb, psi_ub, phi_lb, phi_ub, alp0, alp, beta, A, r0, r, z_input)
+function Robust_Product_Design(N, n_x, n_c, gamma, psi_lb, psi_ub, phi_lb, phi_ub, alp0, alp, beta, A, r0, r, z_input,C_mat)
 
     model = Model(Mosek.Optimizer)
     set_attribute(model, "QUIET", true)
@@ -139,8 +142,8 @@ function Robust_Product_Design(N, n_x, n_c, gamma, psi_lb, psi_ub, phi_lb, phi_u
         @constraint(model, D_[2,:,i] .- z.* z_input[i] .== 0)
     end
 
-    @constraint(model, delta + psi[2] + phi[1] + eta[1] * gamma - alp0 * omega_0[1] - alp' * omega[1,:] - beta' * pi_[1,:] - sum(A .* D_[1,:,:]) <= 0)
-    @constraint(model, delta + psi[1] + phi[2] + eta[2] * gamma - alp0 * omega_0[2] - alp' * omega[2,:] - beta' * pi_[2,:] - sum(A .* D_[2,:,:]) <= r0 + r' * x)
+    @constraint(model, delta + psi[2] + phi[1] + eta[1] * gamma - alp0 * omega_0[1] - alp' * omega[1,:] - beta' * pi_[1,:] - sum(A .* D_[1,:,:]) == 0)
+    @constraint(model, delta + psi[1] + phi[2] + eta[2] * gamma - alp0 * omega_0[2] - alp' * omega[2,:] - beta' * pi_[2,:] - sum(A .* D_[2,:,:]) == r0 + r' * x)
 
     @constraint(model, [psi[3], psi[2], psi[1]] in MOI.DualExponentialCone())
     @constraint(model, [phi[3], phi[2], phi[1]] in MOI.DualExponentialCone())
@@ -148,6 +151,13 @@ function Robust_Product_Design(N, n_x, n_c, gamma, psi_lb, psi_ub, phi_lb, phi_u
     @constraint(model, [eta[1];omega_0[1];omega[1,:];pi_[1,:];vec(D_[1,:,:])] in SecondOrderCone())
     @constraint(model, [eta[2];omega_0[2];omega[2,:];pi_[2,:];vec(D_[2,:,:])] in SecondOrderCone())
 
+    # @variable(model, 0.0 <= eta[1])
+    # @constraint(model, [eta[1];omega_0[1];omega[1,:];pi_[1,:];vec(D_[1,:,:])] in MOI.NormOneCone())
+    # @variable(model, 0.0 <= eta[2])
+    # @constraint(model, [eta[2];omega_0[2];omega[2,:];pi_[2,:];vec(D_[2,:,:])] in MOI.NormOneCone())
+
+
+    # @constraint(model, C_mat * x .== 1)
     @constraint(model, sum(x) >= 1)
     @objective(model, Max, delta)
     optimize!(model)
@@ -167,3 +177,70 @@ function Robust_Product_Design(N, n_x, n_c, gamma, psi_lb, psi_ub, phi_lb, phi_u
     return obj_val, x_val, solve_time
 end
 
+function Robust_Product_Design_Condense_Parameters(N, n_x, gamma, psi_lb, psi_ub, phi_lb, phi_ub, nu0, nu, r0, r)
+
+    model = Model(Mosek.Optimizer)
+    set_attribute(model, "QUIET", true)
+    # 定义变量
+    @variable(model, delta)                           # 标量 δ
+    @variable(model, eta[1:N])  
+    @variable(model, omega_0[1:N])    
+    @variable(model, omega[1:N,1:n_x])  
+    @variable(model, x[1:n_x], Bin)           
+    @variable(model, psi[1:3])                        
+    @variable(model, phi[1:3])             
+    @variable(model, y[1:n_x])        
+    @variable(model, z[1:n_x])  
+
+    @constraint(model, omega_0[1] + psi[3] == 0)
+    @constraint(model, omega[1,:] .+ y .== 0)
+    for ind in 1:n_x
+        @constraint(model, y[ind] >= psi_lb * x[ind])
+        @constraint(model, y[ind] <= psi_ub * x[ind])
+        @constraint(model, y[ind] >= psi[3] - psi_ub * (1 - x[ind]))
+        @constraint(model, y[ind] <= psi[3] - psi_lb * (1 - x[ind]))
+    end
+
+
+    @constraint(model, omega_0[2] - phi[3] == 0)
+    @constraint(model, omega[2,:] .- z .== 0)
+    for ind in 1:n_x
+        @constraint(model, z[ind] >= phi_lb * x[ind])
+        @constraint(model, z[ind] <= phi_ub * x[ind])
+        @constraint(model, z[ind] >= phi[3] - phi_ub * (1 - x[ind]))
+        @constraint(model, z[ind] <= phi[3] - phi_lb * (1 - x[ind]))
+    end
+
+
+    @constraint(model, delta + psi[2] + phi[1] + eta[1] * gamma - nu0 * omega_0[1] - nu' * omega[1,:] == 0)
+    @constraint(model, delta + psi[1] + phi[2] + eta[2] * gamma - nu0 * omega_0[2] - nu' * omega[2,:] == r0 + r' * x)
+
+    @constraint(model, [psi[3], psi[2], psi[1]] in MOI.DualExponentialCone())
+    @constraint(model, [phi[3], phi[2], phi[1]] in MOI.DualExponentialCone())
+
+    # @constraint(model, [eta[1];omega_0[1];omega[1,:]] in SecondOrderCone())
+    # @constraint(model, [eta[2];omega_0[2];omega[2,:]] in SecondOrderCone())
+
+    @constraint(model, 0.0 <= eta[1])
+    @constraint(model, [eta[1];omega_0[1];omega[1,:]] in MOI.NormOneCone(n_x + 2))
+    @constraint(model, 0.0 <= eta[2])
+    @constraint(model, [eta[2];omega_0[2];omega[2,:]] in MOI.NormOneCone(n_x + 2))
+
+    @constraint(model, sum(x) >= 1)
+    @objective(model, Max, delta)
+    optimize!(model)
+
+    status = JuMP.termination_status(model)
+    # println("status: ", status)
+    # solution_summary(model)
+    if status == MOI.OPTIMAL
+        obj_val = objective_value(model)
+        x_val = value.(x)
+        solve_time = JuMP.solve_time(model)
+    else
+        obj_val = NaN
+        x_val = ones(n_x) .* NaN
+        solve_time = NaN
+    end
+    return obj_val, x_val, solve_time
+end
