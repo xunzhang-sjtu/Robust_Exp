f282c59e00e56fd1f48dc11860c557a1aa6df714
xunzhang-sjtu <136031617@qq.com>
2025-10-15 17:16:27 +0800

    add for PP

---
+++ Joint_Pricing_and_Promotion/Params_PP.jl
@@ -0,0 +1,24 @@
+function get_default_params_PP()
+    Params = Dict()
+    Params["N"] = 3
+    Params["N_u"] = 1
+    Params["K"] = 30
+    Params["S_train"] = 100
+    Params["S_test"] = 1000
+    Params["P_bar"] = 30.0
+    Params["iterations"] = 100
+    Params["offdiag_sign"] = "mix"
+    Params["max_offdiag"] = 1.0
+    Params["Time_Limit"] = 180.0
+    Params["gamma_list"] = [0.0,0.01,0.02,0.03,0.04,0.06,0.08]
+    Params["dual_norm"] = 2
+    Params["psi_lb_coef"] = -20.0
+    Params["phi_lb_coef"] = -20.0
+    Params["psi_lb"] = Params["psi_lb_coef"] * ones(Params["N"]) 
+    Params["psi_ub"] = 0.0 * ones(Params["N"]) 
+    Params["phi_lb"] = Params["phi_lb_coef"] * ones(Params["N"]) 
+    Params["phi_ub"] = 0.0 * ones(Params["N"]) 
+    Params["is_original_setting"] = true
+    Params["seed"] = 3
+    return Params
+end
